1a73b868dab9d53f2e03a6e6a4150d3bdf99a287
Viktor Muravev <neuron96@mail.ru>
2022-12-07 19:18:15 +0400

    feat: add types to loader

---
+++ migration-to-ts/src/components/controller/loader.ts
@@ -0,0 +1,55 @@
+import { Errors, IOptions, IResponseArticleObj } from './../../types/index';
+class Loader {
+    baseLink: string;
+    options: IOptions;
+    constructor(baseLink: string, options: IOptions) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp(
+        { endpoint, options }: { endpoint: string; options: IOptions },
+        callback = () => {
+            console.error('No callback for GET response');
+        }
+    ): void {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    private errorHandler(res: Response): Response {
+        console.log('resres', res);
+        if (!res.ok) {
+            if (res.status === Errors.unauthorized || res.status === Errors.notFound)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    private makeUrl({ options, endpoint }: { options: IOptions; endpoint: string }): string {
+        const urlOptions = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    private load(
+        method: 'GET' | 'POST',
+        endpoint: string,
+        callback: (data: IResponseArticleObj) => void,
+        options: IOptions
+    ): void {
+        fetch(this.makeUrl({ options, endpoint }), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
+    }
+}
+
+export default Loader;
