6d495055b02083946e866d2c2b1fa8584a630b91
Viktor Muravev <neuron96@mail.ru>
2022-12-07 15:43:32 +0400

    fix: add API Key

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '40d7e4f08e8b4e0489aa487d328a88f7', // получите свой ключ https://newsapi.org/
         });
     }
 }
