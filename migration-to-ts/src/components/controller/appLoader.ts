01f090d4248c0070e304e8add50c46e53e2cdf80
Viktor Muravev <neuron96@mail.ru>
2022-12-08 21:39:30 +0400

    fix: change proxy server for build

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: '40d7e4f08e8b4e0489aa487d328a88f7', // получите свой ключ https://newsapi.org/
         });
     }
